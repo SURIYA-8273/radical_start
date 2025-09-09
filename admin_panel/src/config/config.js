bd7f9d280584f06ec79b9443e6733ff2c50fcd0f
suriya-8273 <suriyasivakumar60@gmail.com>
2025-09-09 12:15:16 +0530

    fix : online backend url set in frontend

--- admin_panel/src/config/config.js
+++ admin_panel/src/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    // BASE_URL : "https://radical-start.vercel.app"
-    BASE_URL : "http://localhost:3000"
+    BASE_URL : "https://radical-start.vercel.app"
+    // BASE_URL : "http://localhost:3000"
 }
